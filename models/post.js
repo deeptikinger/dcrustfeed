7a9c65773bc0d95e8c072620100faa7401314014
deepti <kingerdeepti123@gmail.com>
2020-09-14 20:37:42 +0530

    changes
    Signed-off-by: deepti <kingerdeepti123@gmail.com>

--- models/post.js
+++ models/post.js
@@ -12,11 +12,7 @@ const postSchema = new mongoose.Schema({
     },
     photo: {
         type: String,
-<<<<<<< HEAD
-        required:true
-=======
         required: true
->>>>>>> 20da01a17537d5356f1d951c4e99e6cd099f195b
     },
     postedBy: {
         type: ObjectId,
