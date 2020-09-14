a77179bdd5e3ad33b438dcf90ab5534880860f10
deepti <kingerdeepti123@gmail.com>
2020-09-14 20:36:01 +0530

    changes
    Signed-off-by: deepti <kingerdeepti123@gmail.com>

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,33 +1,40 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
 const Home = () => {
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        })
+        .then((res)=>{
+          res.json()
+        })
+        .then(result=>{
+            setData(result.posts)
+        })
+    },[])
     return (
         <div className="home">
-           <div className="card home-card">
-               <h5>Name of user</h5>
+            {
+                data.map(item=>{
+                    return(
+                <div className="card home-card" key={item._id}>
+               <h5>{item.postedBy.name}</h5>
                <div className="card-image">
-                   <img src="https://images.unsplash.com/photo-1598900563415-bac8177664aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                   <img src={item.photo}/>
               </div>
               <div className="card-content">
               <i className="material-icons" style={{color:"red"}}>favorite</i>
-                  <h6>title of post</h6>
-                  <p>this is amazing post</p>
+                  <h6>{item.title}</h6>
+                    <p>{item.body}</p>
                   <input type="text" placeholder="addComment"/>
               </div>
            </div>
-           <div className="card home-card">
-               <h5>Name of user</h5>
-               <div className="card-image">
-                   <img src="https://images.unsplash.com/photo-1598900563415-bac8177664aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-              </div>
-              <div className="card-content">
-              <i className="material-icons" style={{color:"red"}}>favorite</i>
-                  <h6>title of post</h6>
-                  <p>this is amazing post</p>
-                  <input type="text" placeholder="addComment"/>
-              </div>
-           </div>
-        </div>
+                )
+            })
+        }
+        </div>  
     )
 }
 
