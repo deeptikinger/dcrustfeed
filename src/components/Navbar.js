2342f40a87c9c7b2702a1f861cb42722d5bfca12
deepti <kingerdeepti123@gmail.com>
2020-09-14 20:22:31 +0530

    changes
    Signed-off-by: deepti <kingerdeepti123@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link ,useHistory} from 'react-router-dom'
+import {UserContext} from '../App'
 const Navbar = () => {
+    const {state,dispatch}=useContext(UserContext)
+    const history=useHistory()
+    const renderList=()=>{
+        if(state){
+        return [
+            <li><Link to="/profile">Profile</Link></li>,
+            <li><Link to="/createpost">Create Post</Link></li>,
+            <button className="btn waves-effect waves-light #ef5350 red lighten-1"
+                    onClick={() =>{
+                        localStorage.clear()
+                        dispatch({type:"CLEAR"})
+                        history.push('/login')
+                    }
+                }
+                >Logout
+            </button>
+        ]
+      }else{
+          return [
+            <li><Link to="/login">Login</Link></li>,
+            <li><Link to="/signup">Signup</Link></li>,
+          ]
+      }
+    }
     return (
         <nav>
             <div className="nav-wrapper white" >
-                <Link to="/" className="brand-logo left">Dcrustfeed</Link>
+                <Link to={state?"/":"/login"} className="brand-logo left">Dcrustfeed</Link>
                 <ul id="nav-mobile" className="right ">
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/signup">Signup</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/createpost">Create Post</Link></li>
+                    {renderList()}
                 </ul>
             </div>
         </nav>
