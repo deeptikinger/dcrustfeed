1a5e96f6ccd751a637a92d6763a13411961c03fe
Himanshi Maurya <mauryahimanshi9898@gmail.com>
2020-09-20 09:46:10 +0530

    solved the bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,37 @@
 import React, { useContext } from 'react'
-import { Link ,useHistory} from 'react-router-dom'
-import {UserContext} from '../App'
+import { Link, useHistory } from 'react-router-dom'
+import { UserContext } from '../App'
 const Navbar = () => {
-    const {state,dispatch}=useContext(UserContext)
-    const history=useHistory()
+    const { state, dispatch } = useContext(UserContext)
+    const history = useHistory()
     console.log(state)
-    const renderList=()=>{
+    const renderList = () => {
         console.log(state)
-        if(state){
-        return [
-            <li><Link to="/profile">Profile</Link></li>,
-            <li><Link to="/createpost">Create Post</Link></li>,
-            <button className="btn waves-effect waves-light #ef5350 red lighten-1"
-                    onClick={() =>{
+        if (state) {
+            return [
+                <li><Link to="/profile"> Profile</Link></li>,
+                <li><Link to="/createpost">Create Post</Link></li>,
+                <button className="btn waves-effect waves-light #ef5350 red lighten-1"
+                    onClick={() => {
                         localStorage.clear()
-                        dispatch({type:"CLEAR"})
+                        dispatch({ type: "CLEAR" })
                         history.push('/login')
                     }
-                }
+                    }
                 >Logout
             </button>
-        ]
-      }else{
-          return [
-            <li><Link to="/login">Login</Link></li>,
-            <li><Link to="/signup">Signup</Link></li>,
-          ]
-      }
+            ]
+        } else {
+            return [
+                <li><Link to="/login">Login</Link></li>,
+                <li><Link to="/signup">Signup</Link></li>,
+            ]
+        }
     }
     return (
         <nav>
             <div className="nav-wrapper white" >
-                <Link to={state?"/":"/login"} className="brand-logo left">Dcrustfeed</Link>
+                <Link to={state ? "/" : "/login"} className="brand-logo left">Dcrustfeed</Link>
                 <ul id="nav-mobile" className="right ">
                     {renderList()}
                 </ul>
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,42 +1,42 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 const Home = () => {
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        fetch('/allpost',{
-            headers:{
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('/allpost', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
             }
         })
-        .then((res)=>{
-          res.json()
-        })
-        .then(result=>{
-            console.log(result)
-            // setData(result.posts)
-        })
-    },[])
+            .then((res) => {
+                res.json()
+            })
+            .then(result => {
+                console.log(result)
+                // setData(result.posts)
+            })
+    }, [])
     return (
         <div className="home">
             {
-                data.map(item=>{
-                return(
-                <div className="card home-card" key={item._id}>
-               <h5>{item.postedBy.name}</h5>
-               <div className="card-image">
-                   <img src={item.photo}/>
-              </div>
-              <div className="card-content">
-              <i className="material-icons" style={{color:"red"}}>favorite</i>
-                  <h6>{item.title}</h6>
-                    <p>{item.body}</p>
-                  <input type="text" placeholder="addComment"/>
-              </div>
-           </div>
-                )
-            })
-        }
-        </div>  
-    )
+                data.map(item => {
+                    return (
+                        <div className="card home-card" key={item._id}>
+                            <h5>{item.postedBy.name}</h5>
+                            <div className="card-image">
+                                <img src={item.photo} />
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="addComment" />
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
 }
 
 export default Home;
