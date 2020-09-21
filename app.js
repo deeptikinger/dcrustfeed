5b5fc42aab840db5aeaa18b8d6a6649dee171a84
deepti <kingerdeepti123@gmail.com>
2020-09-21 16:19:49 +0530

    till comment
    Signed-off-by: deepti <kingerdeepti123@gmail.com>

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Headers',
    'Origin,X-Requested-With,Content-Type,Accept,Authorization')
    if(req.method==='OPTIONS'){
-       res.header('Access-Control-Allow-Method','PUT ,POST ,PATCH,DELETE ,GET')
+       res.header('Access-Control-Allow-Methods','POST,GET,PUT,DELETE')
        return res.status(200).json({})
    }
    next()
