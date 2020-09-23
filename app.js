b1cc79e7cb3d971f46b6124331f5ae07c583d646
Himanshi Maurya <mauryahimanshi9898@gmail.com>
2020-09-23 08:33:58 +0530

    added user route and follow unfollow feature

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const { MONGODB_URL } = require('./keys')
-const morgan=require('morgan')
+const morgan = require('morgan')
 
 mongoose.connect(MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
 
@@ -12,15 +12,15 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log('error while connecting', err)
 })
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*')
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers',
-   'Origin,X-Requested-With,Content-Type,Accept,Authorization')
-   if(req.method==='OPTIONS'){
-       res.header('Access-Control-Allow-Methods','POST,GET,PUT,DELETE')
-       return res.status(200).json({})
-   }
-   next()
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE')
+        return res.status(200).json({})
+    }
+    next()
 })
 require('./models/user')
 require('./models/post')
@@ -29,6 +29,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use('/user', require('./routes/auth'))
 app.use('/post', require('./routes/post'))
+app.use('/client', require('./routes/client'))
 app.listen(5000, () => {
     console.log("server is running")
 })
--- models/user.js
+++ models/user.js
@@ -1,18 +1,22 @@
-const mongoose=require('mongoose')
-const userSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema.Types;
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
         // match:/[0-9]+(?:\.[a-z]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password:{
-        type:String,
-        required:true
-    }
+    password: {
+        type: String,
+        required: true
+    },
+    followers: [{ type: ObjectId, ref: "User" }],
+    following: [{ type: ObjectId, ref: "User" }]
 })
 
-module.exports=mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema)
