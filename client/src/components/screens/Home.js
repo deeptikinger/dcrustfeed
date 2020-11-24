b20aac47a18abc7707e221ff6f785441edc0dce0
deepti <kingerdeepti123@gmail.com>
2020-11-25 00:14:14 +0530

    final deployment
    Signed-off-by: deepti <kingerdeepti123@gmail.com>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -121,8 +121,9 @@ const Home = () => {
                     return (
                         <div className="card home-card" key={item._id}>
                             <h5 style={{padding:"5px"}}><Link to={item.postedBy._id !== state._id ? "/profile/" + item.postedBy._id : "/profile"}>{item.postedBy.name}</Link>{item.postedBy._id == state._id
-                                && <i className="material-icons" style={{
-                                    float: "right"
+                                && <i className="material-icons" 
+                                style={{
+                                    float:"right"
                                 }}
                                     onClick={() => deletePost(item._id)}
                                 >delete</i>
